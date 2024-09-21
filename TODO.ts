16e77a2f51358792e2b7d0296c5e150e623d005f
Assaf Fogelman <assaf.fogelman@gmail.com>
2024-09-21 19:18:19 +0300

    Add TODO.ts for tracking project tasks and improvements fixed ErrorBoundary added safeareaView added .cursorrules

---
+++ TODO.ts
@@ -0,0 +1,18 @@
+/*
+    StackNavigator:
+    * set initial screens
+    * set the navigation to "home"/"chats" in other screens
+
+    Tabs Screen:
+    *    make the tab icons outlined when not selected
+    * change the color of the icons from black to "on primary"
+
+
+    * the websocket needs to reconnect without losing data - take from the websocket site
+    * the buttons - connect, chat
+    * add a menu where the user can see the blocked users, change their profile
+    * add FAB of "looking to"
+    * check again whether the user list in "HomeScreen" actually update every once in a while (It should periodically)
+    * change the "open cage" request to go to the server instead directly to the site to prevent key theft
+    * check why when we get to the ErrorBoundary, the Error log  request fails due to invalid token
+ */
