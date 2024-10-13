2d41ca5e174d315826990cfb468ed5a1f6fbe8da
Assaf Fogelman <assaf.fogelman@gmail.com>
2024-10-13 14:21:11 +0300

    I've put the subscription in LocationContext in order for the interval and fetching to happen in HomeScreen with useQuery. Haven't checked that it works still

--- LocationContext.tsx
+++ LocationContext.tsx
@@ -1,11 +1,17 @@
-import { useTrackLocation } from "./hooks/useTrackLocation";
+// import { useTrackLocation } from "./hooks/useTrackLocation";
 import { knnDataType } from "./types/types";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import * as Location from "expo-location";
+
+// regardless of the location changes, perform the query every LOCATION_INTERVAL
+const LOCATION_INTERVAL = 2 * 60 * 1000; // 2 minutes in milliseconds
+//if the location changes more than DISTANCE_INTERVAL, perform only after TIME_INTERVAL
+const DISTANCE_INTERVAL = 50; // 50 meters
+const TIME_INTERVAL = 30 * 1000; // 30 seconds
 
 type LocationContextType = {
-  knnDataIsLoading: boolean;
-  knnDataIsError: boolean;
-  knnData: knnDataType;
+  subscribe: () => void;
+  currentLocation: Location.LocationObject | null;
 };
 
 export const LocationContext = createContext<LocationContextType | undefined>(
@@ -13,21 +19,68 @@ export const LocationContext = createContext<LocationContextType | undefined>(
 );
 
 export function LocationProvider({ children }: { children: React.ReactNode }) {
-  const { knnDataIsLoading, knnDataIsError, knnData } = useTrackLocation();
+  const { subscribe, currentLocation } = useSubscribeLocation();
   //askLocationPermissionWhenAppReturnsToForeground:
   //in case the user gave a location permission it the phone's settings,
   //and returns to the app, the app should ask him to whether he would like to track his location
   return (
-    <LocationContext.Provider
-      value={{
-        knnDataIsLoading,
-        knnDataIsError,
-        knnData,
-      }}
-    >
+    <LocationContext.Provider value={{ subscribe, currentLocation }}>
       {children}
     </LocationContext.Provider>
   );
+}
+
+function useSubscribeLocation() {
+  const locationSubscription = useRef<Location.LocationSubscription | null>(
+    null
+  );
+  const [currentLocation, setCurrentLocation] =
+    useState<Location.LocationObject | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (locationSubscription.current) {
+        // Intentionally using current ref value at cleanup time
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        locationSubscription.current.remove();
+      }
+    };
+  }, []);
+
+  return { subscribe, currentLocation };
+
+  async function subscribe() {
+    try {
+      if (locationSubscription.current) return;
+
+      //ask for location permission
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        console.error("Permission to access location was denied");
+        return;
+      }
+
+      //for the first time the user subscribes to location updates, we get the last known location
+      const lastKnownLocation = await Location.getLastKnownPositionAsync({});
+      setCurrentLocation(lastKnownLocation);
+
+      //then we start the location subscription for location updates
+      locationSubscription.current = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          distanceInterval: DISTANCE_INTERVAL,
+          timeInterval: TIME_INTERVAL,
+        },
+        handleNewLocation
+      );
+    } catch (error) {
+      console.error("Error in startDeviceMotionTracking:", error);
+    }
+
+    function handleNewLocation(location: Location.LocationObject) {
+      setCurrentLocation(location);
+    }
+  }
 }
 
 export function useLocation() {
